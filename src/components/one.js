554eedeb642293b8895dc11dd6acd54522cfd4a6
zhaofengli <emmieeasy@163.com>
2018-04-13 18:12:41 +0800

    提交代码

---
+++ src/components/one.js
@@ -0,0 +1,13 @@
+import React,{Component} from 'react'
+
+// React.use(Component);
+class One extends React.Component {
+    render(){
+        return (
+            <div>
+
+            </div>
+        );
+    }
+}
+export default One;
